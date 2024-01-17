b76e3446c21126190aa14085c7139ab26f569f02
tim <timdesmet00@gmail.com>
2024-01-17 06:42:41 +0100

    ajout state et verification des checkbox

--- src/assets/components/TodoList.jsx
+++ src/assets/components/TodoList.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 
 export const TodoList = () => {
-    const todos = ["Learn React", "Be Awesome!"];
+    const initialTodos = ["Learn React", "Be Awesome!"];
+    const [todos, setTodos] = useState(initialTodos);
+    const checkboxChange =(event, index) => {
+        console.log(`Checkbox ${index} is checked: ${event.target.checked}`);
+    }
     return (
         <div className="todoList">
             <ul>
-                {todos.map((todo, index) => (
+                {initialTodos.map((todo, index) => (
                 <li key={index}>
-                    <input type="checkbox" />{ todo }
+                    <input type="checkbox" onChange={(event) => checkboxChange(event, index)} />{ todo }
                 </li>
                 ))}
             </ul>
